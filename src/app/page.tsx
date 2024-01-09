af59e9affaa77c890af87eee47c98577f0ca1acd
EdoBergamo <edo.bergamo03@icloud.com>
2024-01-09 10:56:36 -0800

    feat: Developed Jumbotron

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
+import { Jumbotron } from '@/components/Jumbotron'
 import { Navbar } from '@/components/Navbar'
 
 export default function Home() {
   return (
     <main>
       <Navbar />
+      <Jumbotron />
     </main>
   )
 }
